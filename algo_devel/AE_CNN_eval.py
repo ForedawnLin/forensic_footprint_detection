c2506f1436042c6b50a2d4fae78204aa796dc44d
Tong_Lin <tongl1@andrew.cmu.edu>
2018-11-27 17:16:48 -0500

    add AE_CNN_eval

---
+++ algo_devel/AE_CNN_eval.py
@@ -0,0 +1,310 @@
+import numpy as np 
+#import system 
+import glob
+from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D,Dense,Flatten
+from keras.models import Model
+from keras.optimizers import RMSprop
+from keras.preprocessing import image
+from keras.losses import mean_squared_error,categorical_crossentropy 
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+
+import cv2 
+
+### 1. figure out how to add multiple loss 
+### 2. figure out the label file and how to add to data gen kl
+
+
+def autoencoder_CNN(input_img):
+	conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+	conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+	conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+
+	#decoder
+	conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 128
+	up1 = UpSampling2D((2,2))(conv4) # 14 x 14 x 128
+	conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
+	up2 = UpSampling2D((2,2))(conv5) # 28 x 28 x 64
+	decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+
+	# CNN classifier 
+	CNN_pool_1 = MaxPooling2D(pool_size=(2, 2))(conv3) #16 x 16 x 128
+	CNN_conv_1 = Conv2D(128, (16, 16), activation='relu')(CNN_pool_1) #1 x 1 x 64
+	#CNN_pool_2 = MaxPooling2D(pool_size=(2, 2))(CNN_conv_1) #7 x 7 x 64
+	flat1 = Flatten()(CNN_conv_1)
+	#dense1=Dense(1000, activation='relu')(flat1)
+	classifer=Dense(1175, activation='softmax')(flat1)
+	return decoded,classifer
+
+
+
+def AE_loss(y_true, y_pred):
+	### AE loss 
+	y1_pred = y_pred
+	y1_true = y_true
+	#loss1= K.mean(K.square(y1_true-y1_pred))
+	loss1= mean_squared_error(y1_true,y1_pred)
+	# print ('y1_pred:',K.int_shape(y1_pred) )
+	# print ('y1_true:',K.int_shape(y1_true) )
+	# print ('MSE loss1 shape',K.int_shape(loss1))
+	return loss1#{'loss1':loss1,'loss2':loss2}
+
+def Classifier_loss(y_true,y_pred):
+	y2_pred = y_pred
+	y2_true = y_true
+	loss2= categorical_crossentropy(y2_true,y2_pred)
+	# print ('y2_pred:',K.int_shape(y2_pred) )
+	# print ('y2_true:',K.int_shape(y2_true) )
+	# print ('CE loss2 shape',K.int_shape(loss2))
+	#loss=[loss1,loss2]
+	return loss2
+
+
+
+
+### load data ###
+def load_data(main_path,index_path,label_path):
+	pic_fmt='.jpg'  ## picture format 
+	imagePaths_list=[]  ## init image paths
+	FileID_index=open(index_path,'r')
+	for indice in FileID_index:
+		indice=indice.replace(' ','')
+		#indice=indice.replace('r','',1)
+		index=indice.split(',')
+	index=index[:-1]
+	indice=[]
+	for path in index:
+		if 'r' in path:
+			indice.append('r'+path)
+		else:
+			indice.append(path)  
+	#index=['r'+index[i] for i in np.arange(len(index)) if 'r' in index[i]]  ### name issue, for reference image, its named as r+name_in_file
+	#print (indice)
+	FileID_label=open(label_path,'r')
+	for labels in FileID_label:
+		label=labels.split(',')
+	label=label[:-1]
+	#print (label)
+	imagePaths_list=[main_path+indice[i]+pic_fmt for i in np.arange(len(index))]
+	label=[int(label[i])-1 for i in np.arange(len(label))]  ## -1 b/c zero index 
+	return imagePaths_list,label 
+
+mainPath_train='../../FID-300/tracks_cropped/cropped/train/'  ## main path of the pictures 
+train_index_path='../data_augmentation/label_train_index.txt' 
+train_label_path= '../data_augmentation/label_train.txt'
+imagePaths_list_train,label=load_data(mainPath_train,train_index_path,train_label_path)
+
+mainPath_test='../../FID-300/tracks_cropped/cropped/test/'  ## main path of the pictures 
+test_index_path='../data_augmentation/label_test_index.txt' 
+test_label_path= '../data_augmentation/label_test.txt'
+imagePaths_list_test,label_test=load_data(mainPath_test,test_index_path,test_label_path)
+
+print (imagePaths_list_test)
+print ('label_test:',label_test)
+print ('label num:',len(list(set(label))))
+max_label=1175
+
+image_num=np.size(imagePaths_list_train) ### total image numbers 
+
+train_num=image_num
+#train_valid_ratio=4
+#train_num=int(np.floor(image_num/(train_valid_ratio+1)*train_valid_ratio))
+#valid_num=image_num-train_num
+
+
+train_imagePaths_list=imagePaths_list_train[:train_num]
+train_label=label[:train_num]
+#valid_imagePaths_list=imagePaths_list[train_num:image_num]
+#valid_label=label[train_num:image_num]
+valid_imagePaths_list=imagePaths_list_test
+valid_label=label_test
+valid_num=len(valid_label)
+#print ('test_label',label_test)
+print ('valid_image_num', len(valid_imagePaths_list))
+print ('valid_label_num',valid_num)
+
+print ('img_num:',image_num)
+print ('train_num:',train_num)
+#print ('valid_num:',valid_num)
+print ('test_num:',len(valid_label))
+
+
+
+#### trainig settings ####
+train_ord=np.random.permutation(train_num)
+train_random_paths=[train_imagePaths_list[i] for i in train_ord] ### randomize training image paths
+train_random_label=[train_label[i] for i in train_ord]
+#print (train_random_label)
+
+
+batch_size = 32
+iters_batch = int(np.floor(np.true_divide(train_num,batch_size)))
+epochs = 1000	
+
+#n_valid_check=50 ### number of validation images for check at each iteration  
+#valid_batch_size = 50
+valid_batch_size = len(label_test)
+valid_iters_batch = int(np.floor(np.true_divide(valid_num,valid_batch_size)))
+
+
+
+
+n_imgs=batch_size ### input layer image number 
+print(train_imagePaths_list[0])
+img= cv2.imread(train_imagePaths_list[0])[:,:,1]
+#cv2.imshow('img',img)
+#cv2.waitKey()
+img_h=np.shape(img)[0] ### input layer image height 
+img_w=np.shape(img)[1] ### input layer image width
+img_channel=1 ### input layer image width, gray image 	
+print ('imag_shape',img_h,img_w,img_channel)
+
+resize_w=128; ### resize image to before feeding into network 
+resize_h=128;
+input_img = Input(shape = (resize_w, resize_h, img_channel)) ### -2 for maxpool and upsample commendation 
+autoEncoder_CNN = Model(input_img, autoencoder_CNN(input_img)) ### create model 
+#sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+autoEncoder_CNN.compile(loss=[AE_loss,Classifier_loss],optimizer = 'Adagrad',loss_weights=[0.5, 0.5])
+print ('metric_name:',autoEncoder_CNN.metrics_names)
+#autoEncoder_CNN.summary()
+
+
+def generate_data(img_paths_list,label_list,total_image_num,batch_size,w,h,max_label):
+	### img_paths_list: list contains paths for images 
+	### label_list: associated label list 
+	### total_image_num: len(paths_list), the total images in the list 
+	### batch_size: the batch size 
+	### w,h: resize size for input imgage  
+	### max_label: the lagest label num, define the one-hot vector dimension 
+	image_batch=[] ### batched image 
+	label_batch=[] ### asscociated batched labels 
+	i=0
+	for index in np.arange(batch_size):
+		img_path=img_paths_list[i]
+		label=label_list[i]
+		i+=1;			
+		img= cv2.imread(img_path)[:,:,1]
+		# print ('img',np.shape(img))
+		img= cv2.resize(img,(w,h))/255  ### -2 for maxpool and upsample commendation 
+		img= np.reshape(img,(np.shape(img)[0],np.shape(img)[1],1)) ## instead of m*n, reshape img to 1*m*n*1 for keras input 
+		image_batch.append(img)
+		label_vector= np.zeros(max_label)
+		# print (np.shape(label_vector))
+		label_vector[label]=1
+		label_batch.append(label_vector)
+	image_batch=np.array(image_batch)
+	label_batch=np.array(label_batch)
+	#print (np.shape(label_batch))
+	return image_batch, image_batch,label_batch ##(input, output)
+
+
+
+
+#### Evaluation #######
+		
+autoEncoder_CNN.load_weights('models/AE_CNN_model_weights.41-1.53.hdf5')
+
+
+### test eval #######
+
+######## label check ##########
+data_x,_,_=generate_data(valid_imagePaths_list,valid_label,valid_num,valid_batch_size,resize_w,resize_h,max_label) 
+img_pred,y_pred=autoEncoder_CNN.predict(data_x, batch_size=None, verbose=0, steps=None)
+
+y_pred=np.argmax(y_pred,1)
+print (np.shape(img_pred))
+acc=np.divide(np.sum(y_pred==valid_label),len(y_pred))
+print ('test_acc:',acc)
+wrong_index=[valid_label[i] for i in np.arange(len(y_pred)) if y_pred[i]!=valid_label[i]]
+right_index=[valid_label[i] for i in np.arange(len(y_pred)) if y_pred[i]==valid_label[i]]
+print ('wrong class',wrong_index)
+print ('right class',right_index)
+print ('intersection:',set(wrong_index).intersection(set(right_index)))
+print ('entire comparison',y_pred==valid_label)
+print ('entire label',valid_label)
+compare=y_pred==valid_label
+right_list=[valid_imagePaths_list[i] for i in np.arange(len(y_pred)) if compare[i]==True]
+wrong_list=[valid_imagePaths_list[i] for i in np.arange(len(y_pred)) if compare[i]==False]
+print ('right_classification_num:',len(right_list),'wrong_classification_num:',len(wrong_list))
+
+# for right_img_path in right_list:
+# 	right_img=cv2.imread(right_img_path)
+# 	cv2.imshow('right_img',right_img)
+# 	cv2.waitKey()
+
+# for wrong_img_path in wrong_list:
+# 	wrong_img=cv2.imread(wrong_img_path)
+# 	cv2.imshow('wrong_img',wrong_img)
+# 	cv2.waitKey()
+
+
+####### reconstruction check #########
+for i in (14,15):
+	print (valid_imagePaths_list[i])
+	img2check=i ### the image in test set to check 
+	img_orig= cv2.imread(valid_imagePaths_list[img2check])[:,:,1]
+	img_orig= cv2.resize(img_orig,(128,128))/255  ### -2 for maxpool and upsample commendation 
+	#img_pred=img_pred[img2check,:,:,0]
+	#img_pred=np.squeeze(img_pred)
+	cv2.imshow('orig',img_orig)
+	#cv2.imshow('recons',img_pred)
+	cv2.waitKey()
+
+for label in wrong_index:
+	print ('wrong class freq:',str(label),np.sum(train_label==label*np.ones(len(train_label))))
+
+for label in right_index:
+	print ('right class freq:',str(label),np.sum(train_label==label*np.ones(len(train_label))))
+
+
+
+# ##### train eval ##########
+
+# ######## label check ##########
+# num2check=100
+# data_x_train,_,_=generate_data(train_random_paths,train_random_label,train_num,num2check,resize_w,resize_h,max_label) 
+# img_pred_train,y_pred_train=autoEncoder_CNN.predict(data_x_train, batch_size=None, verbose=0, steps=None)
+
+# y_pred_train=np.argmax(y_pred_train,1)
+# train_label=train_random_label[:num2check]
+# acc=np.divide(np.sum(y_pred_train==train_label),len(y_pred_train))
+# print ('test_acc:',acc)
+# wrong_index_train=[train_label[i] for i in np.arange(len(y_pred_train)) if y_pred_train[i]!=train_label[i]]
+# print ('wrong class',wrong_index_train)
+# print ('entire comparison',y_pred_train==train_label)
+
+
+
+# ####### reconstruction check #########
+# img2check=3; ### the image in test set to check 
+# img_orig_train= cv2.imread(train_random_paths[img2check])[:,:,1]
+# img_orig_train= cv2.resize(img_orig_train,(128,128))/255  ### -2 for maxpool and upsample commendation 
+# img_pred_train=img_pred_train[img2check,:,:,0]
+# img_pred_train=np.squeeze(img_pred_train)
+# cv2.imshow('orig_train',img_orig_train)
+# cv2.imshow('recons_train',img_pred_train)
+# cv2.waitKey()
+
+
+
+
+
+
+
+
+
+# checkpoint
+# filepath="models/AE_CNN_model_weights.{epoch:02d}-{val_loss:.2f}.hdf5"
+# checkpoint = ModelCheckpoint(filepath, monitor='val_dense_1_loss', verbose=1, save_best_only=True, mode='min',save_weights_only=False) ### save model based on classification loss 
+# callbacks_list = [checkpoint]
+
+
+# autoEncoder_CNN.fit_generator(generator=generate_data(train_random_paths,train_random_label,train_num,batch_size,resize_w,resize_h,max_label),
+#                      steps_per_epoch=iters_batch, epochs=epochs,validation_data=generate_data(valid_imagePaths_list,valid_label,valid_num,valid_batch_size,resize_w,resize_h,max_label),validation_steps=valid_iters_batch,callbacks=callbacks_list,shuffle=True)
+
+
+# autoEncoder_CNN.save('models/AE_CNN_model.h5')
+# autoEncoder_CNN.save_weights("models/AE_CNN_model_weights.h5")
+
